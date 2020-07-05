1a4244d14e7e2a7002dd4d5007f4693ffb7c4873
pm <codersland@gmail.com>
2020-07-05 12:36:10 -0500

    with go formating

--- main.go
+++ main.go
@@ -1,3 +1,7 @@
+// Write a program for Tiny URL
+// The program accepts a lengthy url for a website and converts in to a tiny url and stores it
+// when you request with tiny url id it will redirct you the the original site representing the tiny URL
+// Also our service needs to handle multiple concurrent requests so we need to design to avoid concurrency issues.
 package main
 
 import (
@@ -5,53 +9,46 @@ import (
 	"sync"
 )
 
-// Write a program for Tiny URL
-// The program accepts a lengthy url for a website and converts in to a tiny url and stores it
-// when you request with tiny url id it will redirct you the the original site representing the tiny URL
-// Also our service needs to handle multiple concurrent requests so we need to design to avoid concurrency issues.
-
 type URLholder struct {
- tinyrul map [string]string
- mu  sync.RWMutex
+	tinyrul map[string]string
+	mu      sync.RWMutex
 }
 
-func(s *URLholder) Get(keyv string ) string  {
+func (s *URLholder) Get(keyv string) string {
 
 	s.mu.RLock()
 	defer s.mu.Unlock()
-	url:= s.tinyrul[keyv]
+	url := s.tinyrul[keyv]
 
 	return url
 }
 
-func (s *URLholder) Set(value string) bool{
+func (s *URLholder) Set(value string) bool {
 
-   s.mu.Lock()
-   defer s.mu.Unlock()
+	s.mu.Lock()
+	defer s.mu.Unlock()
 
-   key := getkey(value)
-   _,present := s.tinyrul[key]
+	key := getkey(value)
+	_, present := s.tinyrul[key]
 
-   if present{
+	if present {
 
-   	return false
+		return false
 
-   }
+	}
 
-   s.tinyrul[key] = value
+	s.tinyrul[key] = value
 
 	return true
 }
 
-func getkey (val string) string  {
+func getkey(val string) string {
 
 	key := val[:1]
 
-    return key
+	return key
 }
 
 func main() {
 	fmt.Println("Hello..world")
 }
-
-
